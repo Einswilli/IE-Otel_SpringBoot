541bb97abd0aa7bc84e07a5b7cab059f741a6be6
Einswilli <wilfriedgoeh@gmail.com>
2022-01-10 13:05:16 +0000

    initial commit

---
+++ src/test/java/com/ielab/ieotel_springboot/IeotelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ielab.ieotel_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeotelSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
